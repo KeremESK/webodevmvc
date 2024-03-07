fec61ad1be45fd6fcf933aec566345b6e64b67a8
KeremESK <krmesk007@gmail.com>
2024-03-07 22:04:09 +0300

    Proje dosyası ekle.

---
+++ webodevmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webodevmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ webodevmvc/Models/Urun.cs
@@ -0,0 +1,11 @@
+﻿namespace webodevmvc.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Isım { get; set; }
+        public string Aciklama { get; set; }
+        public string Kategori { get; set; }
+        public int Fiyat { get; set; }
+    }
+}
